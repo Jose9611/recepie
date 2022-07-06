9a88b8f5e4b5eca2abbd4402d87f6a2a82293ca4
Jose <joseaugustinejy@gmail.com>
2022-07-06 11:34:15 +0530

    changes in settings of database config

--- app/app/settings.py
+++ app/app/settings.py
@@ -47,8 +47,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
