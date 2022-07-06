24ae8ca57dc20cc0cd743c4bd8467129ec9d257a
Jose <joseaugustinejy@gmail.com>
2022-07-06 11:52:03 +0530

    changes in settings of database config 6

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,10 +5,8 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to wait for database."""
     def handle(self, *args, **options):
         pass
-        """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
