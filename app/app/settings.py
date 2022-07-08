46ff9c6339cea33ec7e8bfc1676f50df15eca254
Jose <joseaugustinejy@gmail.com>
2022-07-08 12:51:00 +0530

    created user-add,updtae,tokengenerate apis with unit tests

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,7 +13,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -81,3 +84,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'core.User'
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
