52f2492868accb1986acd6d4926890810850d16f
jose <jose.augustine@skizzle.tech>
2025-01-03 21:19:28 +0530

    category listing with filters

--- app/category/urls.py
+++ app/category/urls.py
@@ -6,4 +6,5 @@ app_name = 'user'
 urlpatterns = [
     path('create/', views.CreateCategoryView.as_view(), name='create'),
     path('update/<int:pk>/', views.CategoryUpdateView.as_view(), name='update'),
+    path('category-list/', views.CategoryListView.as_view(), name='category-list'),
 ]
